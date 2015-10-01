118754fe31db8939302a188727d0624639b147fa
adriannabanana <afu@NN123-HA-17.HS.sd203.org>
2015-10-01 15:09:07 -0500

    stuff

--- AvgOfThree/ChangeGiva.java
+++ AvgOfThree/ChangeGiva.java
@@ -7,12 +7,39 @@ public class ChangeGiva
         Scanner s = new Scanner (System.in);
         System.out.print("Amount due: ");
         float due = s.nextFloat();
-        due *= 100;
         System.out.print("Amount received: ");
         float received = s.nextFloat();
-        received *= 100;
         System.out.println();
-        if (received > due)
-        {
-            int dollarChange = received % due;
+        float actualChange = received - due;
+        int change = (int) (actualChange*100.0);
+        
+        int dollars = change/100;
+        int dollarRemainder = change%100;
+        int quarters = dollarRemainder/25;
+        int quarterRemainder = dollarRemainder%25;
+        int dimes = quarterRemainder/10;
+        int dimeRemainder = quarterRemainder%10;
+        int nickels = dimeRemainder/5;
+        int nickelRemainder = dimeRemainder%5;
+        int pennies = nickelRemainder;
+        
+        System.out.println();
+        System.out.print("Dollars of change: ");
+        System.out.print(dollars);
+        System.out.println();
+        System.out.print("Quarters of change: ");
+        System.out.print(quarters);
+        System.out.println();
+        System.out.print("Dimes of change: ");
+        System.out.print(dimes);
+        System.out.println();
+        System.out.print("Nickels of change: ");
+        System.out.print(nickels);
+        System.out.println();
+        System.out.print("Pennies of change: ");
+        System.out.print(pennies);
+    }
+}
+        
+                
             
